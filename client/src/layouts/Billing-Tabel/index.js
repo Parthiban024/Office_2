e6095c3489f301157c9c5079fd5757353e1ffe26
madhuhari188 <madhu.hari.188@gmail.com>
2022-08-08 12:51:52 +0530

    new

--- client/src/layouts/Billing-Tabel/index.js
+++ client/src/layouts/Billing-Tabel/index.js
@@ -11,6 +11,9 @@ import Grid from "@mui/material/Grid";
 import DashboardLayout from "examples/LayoutContainers/DashboardLayout";
 import DashboardNavbar from "examples/Navbars/DashboardNavbar";
 import Footer from "examples/Footer";
+import axios from "axios";
+import moment from "moment";
+
 
 const columns = [
   { id: 'date', label: 'Date', minWidth: 100 },
@@ -44,26 +47,47 @@ function createData(date,team,batch,aannotation, aqc, apmsme,atotal,hannotation,
   return {date,team,batch,aannotation, aqc, apmsme,atotal,hannotation, hqc, hpmsme,hprojectTraning,hojt,hqualityfeedback,hidleHours,hother,hcomments,htotal,jannotation,jqc,jtotal };
 }
 
-const rows = [
-  createData("05/08/2022","dumbeldore","batch2","220", "50", "33","220", "54", "33","220", "54", "33","220", "54", "33","220", "54", "33","22","23"),
-  createData("06/08/2022","nala","batch2","220", "54", "33","220", "54", "33","220", "54", "33","220", "54", "33","220", "54", "33","22","23"),
-  createData("07/08/2022","Annatol","batch2","220", "56", "33","220", "54", "33","220", "54", "33","220", "54", "33","220", "54", "33","22","23"),
-  createData("08/08/2022","Annatol","batch2","220", "56", "33","220", "54", "33","220", "54", "33","220", "54", "33","220", "54", "33","22","23"),
-  createData("09/08/2022","Annatol","batch2","220", "56", "33","220", "54", "33","220", "54", "33","220", "54", "33","220", "54", "33","22","23"), 
-  createData("10/08/2022","Annatol","batch2","220", "56", "33","220", "54", "33","220", "54", "33","220", "54", "33","220", "54", "33","22","23"),
-  createData("11/08/2022","Annatol","batch2","220", "56", "33","220", "54", "33","220", "54", "33","220", "54", "33","220", "54", "33","22","23"),
-  createData("12/08/2022","Annatol","batch2","220", "56", "33","220", "54", "33","220", "54", "33","220", "54", "33","220", "54", "33","22","23"),
-  createData("13/08/2022","Annatol","batch2","220", "56", "33","220", "54", "33","220", "54", "33","220", "54", "33","220", "54", "33","22","23"),
+const rowData = [];
 
-];
+axios.get("/billing/")
+.then((res)=>rowData[res.data])
+.catch(err=>console.log(err))
+console.log(rowData)
+// const rows = [
+//   createData("05/08/2022","dumbeldore","batch2","220", "50", "33","220", "54", "33","220", "54", "33","220", "54", "33","220", "54", "33","22","23"),
+//   createData("06/08/2022","nala","batch2","220", "54", "33","220", "54", "33","220", "54", "33","220", "54", "33","220", "54", "33","22","23"),
+//   createData("07/08/2022","Annatol","batch2","220", "56", "33","220", "54", "33","220", "54", "33","220", "54", "33","220", "54", "33","22","23"),
+//   createData("08/08/2022","Annatol","batch2","220", "56", "33","220", "54", "33","220", "54", "33","220", "54", "33","220", "54", "33","22","23"),
+//   createData("09/08/2022","Annatol","batch2","220", "56", "33","220", "54", "33","220", "54", "33","220", "54", "33","220", "54", "33","22","23"), 
+//   createData("10/08/2022","Annatol","batch2","220", "56", "33","220", "54", "33","220", "54", "33","220", "54", "33","220", "54", "33","22","23"),
+//   createData("11/08/2022","Annatol","batch2","220", "56", "33","220", "54", "33","220", "54", "33","220", "54", "33","220", "54", "33","22","23"),
+//   createData("12/08/2022","Annatol","batch2","220", "56", "33","220", "54", "33","220", "54", "33","220", "54", "33","220", "54", "33","22","23"),
+//   createData("13/08/2022","Annatol","batch2","220", "56", "33","220", "54", "33","220", "54", "33","220", "54", "33","220", "54", "33","22","23"),
+
+// ];
+const rows = [
+  rowData.map((item,index)=>{
+    createData(item.reportDate,item.team,item.batch,item.associated.annotation,item.associated.qc,item.associated.pm,
+      item.associated.total,item.hours.annotation,item.hours.qc,item.hours.pm,item.hours.training,item.hours.otj,
+      item.hours.qcFeedback,item.hours.other,item.hours.total,item.jobs.annotation,item.jobs.qc,
+      item.jobs.total)
+  })
+]
 
 export default function ColumnGroupingTable() {
   const [page, setPage] = React.useState(0);
   const [rowsPerPage, setRowsPerPage] = React.useState(10);
+  const [data,setData] = React.useState([])
 
   const handleChangePage = (event, newPage) => {
     setPage(newPage);
   };
+
+  React.useEffect(()=>{
+    axios.get("/billing/")
+    .then((res)=>setData(res.data))
+    .catch(err=>console.log(err))
+  },[])
 
   const handleChangeRowsPerPage = (event) => {
     setRowsPerPage(+event.target.value);
@@ -102,21 +126,42 @@ export default function ColumnGroupingTable() {
             </TableRow>
           </TableHead>
           <TableBody>
-            {rows
+            {data
               .slice(page * rowsPerPage, page * rowsPerPage + rowsPerPage)
-              .map((row) => {
+              .map((item,index) => {
                 return (
-                  <TableRow hover role="checkbox" tabIndex={-1} key={row.aqc}>
-                    {columns.map((column) => {
+                  <TableRow hover role="checkbox" tabIndex={-1} key={index}>
+                    {/* {columns.map((column) => {
                       const value = row[column.id];
                       return (
                         <TableCell key={column.id} align={column.align}>
-                          {/* {column.format && typeof value === 'number'
-                            ? column.format(value) */}
                         { value}
                         </TableCell>
                       );
-                    })}
+                    })} */}
+                    <TableCell>
+                      {moment(item.reportDate).format("DD/MM/YYYY")}
+                    </TableCell>
+    <TableCell>{item.team}</TableCell>
+    <TableCell>{item.batch}</TableCell>
+    <TableCell>{item.associated.annotation}</TableCell>
+    <TableCell>{item.associated.qc}</TableCell>
+    <TableCell>{item.associated.pm}</TableCell>
+    <TableCell>{item.associated.total}</TableCell>
+    <TableCell>{item.hours.annotation}</TableCell>
+    <TableCell>{item.hours.qc}</TableCell>
+    <TableCell>{item.hours.pm}</TableCell>
+    <TableCell>{item.hours.training}</TableCell>
+    <TableCell>{item.hours.ojt}</TableCell>
+    <TableCell>{item.hours.qcFeedback}</TableCell>
+    <TableCell>{item.hours.idle}</TableCell>
+    <TableCell>{item.hours.other}</TableCell>
+    <TableCell>{item.hours.comments}</TableCell>
+    <TableCell>{item.hours.total}</TableCell>
+    <TableCell>{item.jobs.annotation}</TableCell>
+    <TableCell>{item.jobs.qc}</TableCell>
+    <TableCell>{item.jobs.total}</TableCell>
+    <TableCell>Edit | Delete </TableCell>
                   </TableRow>
                 );
               })}
